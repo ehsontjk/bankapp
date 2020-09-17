878528e014882c91e38f58da3059474249154043
Ekhsondzhon Boboev <ehsonboboev@gmail.com>
2020-09-17 19:14:45 +0500

    добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,28 @@
+package types
+type Money int64
+type Balance int64
+type Currency string
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+type PAN string
+type Card struct {
+	ID int
+	PAN PAN
+	Currency Currency
+	Balance Money
+	Color string
+	Name string
+	Active bool
+	MinBalance Money
+	
+
+    
+}
+type PaymentSource struct {
+	Type string // 'card'
+	Number string // номер вида '5058 xxxx xxxx 8888'
+	Balance Money // баланс в дирамах
+}
